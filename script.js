e6e20f20101c9d4a99b7f8ae41c1465b6c2366b3
Cauã Cada <cauacadadealmeida@gmail.com>
2026-06-25 16:35:59 -0300

    refactor: padronização da seção projetos

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Js do about*/
+
 const hiddenElements = document.querySelectorAll(
     ".about-img, .about-content"
 );
@@ -20,3 +22,41 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+/* Js do projetos*/
+
+
+
+const botoesFiltro = document.querySelectorAll(".filtro-btn");
+const projetos = document.querySelectorAll(".projeto-card");
+
+botoesFiltro.forEach(botao => {
+
+    botao.addEventListener("click", () => {
+
+        botoesFiltro.forEach(btn =>
+            btn.classList.remove("active")
+        );
+
+        botao.classList.add("active");
+
+        const filtro = botao.dataset.filter;
+
+        projetos.forEach(projeto => {
+
+            if (
+                filtro === "all" ||
+                projeto.classList.contains(filtro)
+            ) {
+                projeto.style.display = "block";
+            } else {
+                projeto.style.display = "none";
+            }
+
+        });
+
+    });
+
+});
